964d2c17bd19cab65132f0ebb32ee4fbe6c7b8e0
poizxc <poizxc@users.noreply.github.com>
2020-07-27 00:25:13 +0200

    Updates

---
+++ precache-manifest.19fed281ca87cde4db14411293410ff8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5699edb64a992ced8e8b9b4c6e761080",
+    "url": "/agile-board-front/index.html"
+  },
+  {
+    "revision": "6cc296e3c69fcdd76846",
+    "url": "/agile-board-front/static/js/2.049acc7a.chunk.js"
+  },
+  {
+    "revision": "c80b09f21c3dd2137be347c65d78f97d",
+    "url": "/agile-board-front/static/js/2.049acc7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61c205d16e18b59f7f99",
+    "url": "/agile-board-front/static/js/main.6a2f1b71.chunk.js"
+  },
+  {
+    "revision": "b6579b7d9eeb11ea5a8f",
+    "url": "/agile-board-front/static/js/runtime-main.014ebaff.js"
+  }
+]);

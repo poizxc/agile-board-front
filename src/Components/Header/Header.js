3b5088da3ffca33f596785ff984058135e75cbdd
poizxc <poizxc@users.noreply.github.com>
2020-07-30 00:03:27 +0200

    added api sleep info

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Paper, Box, Typography } from '@material-ui/core';
+import useStyles from './HeaderStyles';
 
-const Header = () => (
-  <Paper variant="elevation">
-    <Box p={2}>
-      <Typography variant="h4" align="center">
-        Recruitment Task - Agile Board
-      </Typography>
-    </Box>
-  </Paper>
-);
+const Header = () => {
+  const classes = useStyles();
+
+  return (
+    <Paper variant="elevation">
+      <Box p={2}>
+        <Typography variant="h1" className={classes.heading} align="center">
+          Recruitment Task - Agile Board
+        </Typography>
+        <Typography variant="body2" className={classes.smallInfo} align="center">
+          The first data loading can take a while - api goes to sleep after 30 minutes in idle mode
+        </Typography>
+      </Box>
+    </Paper>
+  );
+};
 
 export default Header;

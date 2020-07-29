0def0cd4fd8d4f9f1d208f7078552a2610109b80
poizxc <poizxc@users.noreply.github.com>
2020-07-29 23:59:28 +0200

    added info about api sleep

---
+++ src/Components/Header/HeaderStyles.js
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+  smallInfo: {
+    fontSize: '0.55rem',
+  },
+  heading: {
+    fontSize: '2.2rem',
+  },
+}));
+export default useStyles;
